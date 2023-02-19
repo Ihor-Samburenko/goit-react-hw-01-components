cb7bc3dffcc45eca490aac66a8957a908c3b98bb
Ihor-Samburenko <samburenko.igor@gmail.com>
2023-02-19 12:30:46 +0200

    change name import

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { Profile } from './components/Profile/Profile';
 import { Statistics } from './components/Statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
 import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
-import User from './data/user.json';
-import Stats from './data/stats.json';
+import user from './data/user.json';
+import stats from './data/stats.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile {...User} />
-      <Statistics stats={Stats} title="Upload stats" />
-      <Statistics stats={Stats} />
+      <Profile {...user} />
+      <Statistics stats={stats} title="Upload stats" />
+      <Statistics stats={stats} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
